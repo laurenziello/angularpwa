719273a06a7cb453f2e8a4f4707ce2f07eb3fdb2
cesare <laurenziello@hotmail.it>
2018-04-09 17:34:31 +0200

    Notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'notifiche';
+  test: any;
+
+  private VAPID_PUBLIC_KEY = 'BDNmu_6uDPDF1hj0UdH8TXTJrF3NDU2jBBFVj-wK3LO4_19eptc4_kHXdix3Ejm0pw6zbTwxjay9SfTER3yGExI';
+  constructor(private swPush: SwPush) {
+
+  }
+
+  ngOnIn() {
+
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+      .then(pushSubscription => {
+        this.test = pushSubscription;
+        // Passing subscription object to our backend
+        console.error(pushSubscription);
+
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+
 }
